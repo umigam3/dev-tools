0ad7f55779549b1cf58d7aa03f0d8f4c0d68bd43
Gerard Doncel <gerarddoncel41@gmail.com>
2024-07-04 23:26:23 +0200

    data.json server side

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,18 @@
-"use client"
+import fs from 'fs';
+import path from 'path';
 
-import { useEffect, useState } from 'react';
-import Header from '../components/header';
+async function getData() {
+  const filePath = path.join(process.cwd(), 'src', 'data', 'data.json');
+  const jsonData = fs.readFileSync(filePath, 'utf-8');
+  return JSON.parse(jsonData);
+}
 
-export default function Home() {
-
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch('/data.json')
-      .then((response) => response.json())
-      .then((json) => setData(json));
-  }, []);
-
-  if (!data) return <div>Loading...</div>;
+export default async function Page() {
+  const data = await getData();
 
   return (
-    <div className="card-container">
-      <Header />
-      {data.map((tool, index) => (
-        <div className="card" key={index}>
-          <h2>{tool.name}</h2>
-          <p>{tool.description}</p>
-          <p>Tags: {tool.tags.join(', ')}</p>
-          <a href={tool.url} target="_blank" rel="noopener noreferrer">Visit {tool.name}</a>
-        </div>
-      ))}
+    <div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
