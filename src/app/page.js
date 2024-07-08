fe149a690c65f59e7a7e15ae92051ff89d4163d1
Gerard Doncel <gerarddoncel41@gmail.com>
2024-07-08 21:18:34 +0200

    added tag filter

--- src/app/page.js
+++ src/app/page.js
@@ -6,11 +6,10 @@ import About from '@/components/About';
 import Card from '@/components/Card';
 
 export default function HomePage() {
-
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [searchQuery, setSearchQuery] = useState('');
-
+  const [selectedTags, setSelectedTags] = useState([]);
 
   useEffect(() => {
     const fetchTools = async () => {
@@ -21,40 +20,62 @@ export default function HomePage() {
 
       setData(tools);
       setIsLoading(false);
-    }
+    };
 
     fetchTools();
-  }, [])
+  }, []);
 
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
   };
 
+  const toggleTag = (tag) => {
+    setSelectedTags((prevTags) =>
+      prevTags.includes(tag)
+        ? prevTags.filter((t) => t !== tag)
+        : [...prevTags, tag]
+    );
+  };
+
   const filteredData = data
     ? data.filter((tool) =>
-        tool.name.toLowerCase().includes(searchQuery.toLowerCase())
+        tool.name.toLowerCase().includes(searchQuery.toLowerCase()) &&
+        (selectedTags.length === 0 || selectedTags.every(tag => tool.tags.includes(tag)))
       )
     : [];
+
+  const allTags = data ? [...new Set(data.flatMap(tool => tool.tags))] : [];
 
   return (
     <main className='max-w-5xl mx-auto px-9'>
       <Hero />
       <section className='flex flex-wrap justify-between min-h-full mx-auto'>
         <div className='w-full mb-5'>
-            <input
-              type='text'
-              placeholder='Search by tool name...'
-              value={searchQuery}
-              onChange={handleSearchChange}
-              className='w-full px-4 py-2 border border-gray-700 rounded-md'
-            />
+          <input
+            type='text'
+            placeholder={`Search among ${filteredData.length} items...`}
+            value={searchQuery}
+            onChange={handleSearchChange}
+            className='w-full px-4 py-2 border border-gray-700 rounded-md'
+          />
+        </div>
+        <div className='w-full mb-5 flex flex-wrap gap-2'>
+          {allTags.map((tag) => (
+            <button
+              key={tag}
+              onClick={() => toggleTag(tag)}
+              className={`px-3 py-1 rounded-3xl border-gray-600 border text-sm ${selectedTags.includes(tag) ? 'bg-white text-black' : 'bg-[#161616] text-white'}`}
+            >
+              {tag}
+            </button>
+          ))}
         </div>
         {isLoading && <div>Loading...</div>}
         {!isLoading && filteredData.map((tool) => (
-          <Card tool={tool} />
+          <Card key={tool.name} tool={tool} />
         ))}
         {filteredData.length === 0 && 
-          <div className='flex items-center justify-center h-48 w-full text-4xl font-bold opacity-20 text-center'>No Tools Founded</div>
+          <div className='flex items-center justify-center h-48 w-full text-4xl font-bold opacity-20 text-center'>No Tools Found</div>
         }
       </section>
       <About />
