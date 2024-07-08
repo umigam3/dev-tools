abbde308e1dc17e012ceb3f8cbd570131b65f155
Gerard Doncel <gerarddoncel41@gmail.com>
2024-07-08 22:01:03 +0200

    added button to show all, added first real tools

--- src/app/page.js
+++ src/app/page.js
@@ -10,13 +10,15 @@ export default function HomePage() {
   const [isLoading, setIsLoading] = useState(false);
   const [searchQuery, setSearchQuery] = useState('');
   const [selectedTags, setSelectedTags] = useState([]);
+  const [initialView, setInitialView] = useState(true);
+  const [showAllButton, setShowAllButton] = useState(false); // Estado para controlar la visibilidad del botón
 
   useEffect(() => {
     const fetchTools = async () => {
       setIsLoading(true);
 
       const response = await fetch('/api/data');
-      const tools = (await response.json());
+      const tools = await response.json();
 
       setData(tools);
       setIsLoading(false);
@@ -27,6 +29,7 @@ export default function HomePage() {
 
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
+    setInitialView(false); // Desactivar la vista inicial cuando se realiza una búsqueda
   };
 
   const toggleTag = (tag) => {
@@ -35,16 +38,31 @@ export default function HomePage() {
         ? prevTags.filter((t) => t !== tag)
         : [...prevTags, tag]
     );
+    setInitialView(false); // Desactivar la vista inicial cuando se selecciona un tag
+  };
+
+  const showAllItems = () => {
+    setInitialView(false); // Desactivar la vista inicial al hacer clic en el botón
+    setShowAllButton(false); // Ocultar el botón después de hacer clic
   };
 
   const filteredData = data
-    ? data.filter((tool) =>
-        tool.name.toLowerCase().includes(searchQuery.toLowerCase()) &&
-        (selectedTags.length === 0 || selectedTags.every(tag => tool.tags.includes(tag)))
+    ? data.filter(
+        (tool) =>
+          tool.name.toLowerCase().includes(searchQuery.toLowerCase()) &&
+          (selectedTags.length === 0 ||
+            selectedTags.every((tag) => tool.tags.includes(tag)))
       )
     : [];
 
-  const allTags = data ? [...new Set(data.flatMap(tool => tool.tags))] : [];
+  const displayedData = initialView ? filteredData.slice(0, 6) : filteredData;
+
+  const allTags = data ? [...new Set(data.flatMap((tool) => tool.tags))] : [];
+
+  useEffect(() => {
+    // Mostrar el botón solo si estamos en la vista inicial y hay más de 5 elementos filtrados
+    setShowAllButton(initialView && filteredData.length > 5);
+  }, [initialView, filteredData]);
 
   return (
     <main className='max-w-5xl mx-auto px-9'>
@@ -64,20 +82,34 @@ export default function HomePage() {
             <button
               key={tag}
               onClick={() => toggleTag(tag)}
-              className={`px-3 py-1 rounded-3xl border-gray-600 border text-sm ${selectedTags.includes(tag) ? 'bg-white text-black' : 'bg-[#161616] text-white'}`}
+              className={`px-3 py-1 rounded-3xl border-gray-700 border text-sm ${
+                selectedTags.includes(tag)
+                  ? 'bg-white text-black'
+                  : 'bg-[#161616] text-white'
+              }`}
             >
               {tag}
             </button>
           ))}
         </div>
         {isLoading && <div>Loading...</div>}
-        {!isLoading && filteredData.map((tool) => (
-          <Card key={tool.name} tool={tool} />
-        ))}
-        {filteredData.length === 0 && 
-          <div className='flex items-center justify-center h-48 w-full text-4xl font-bold opacity-20 text-center'>No Tools Found</div>
-        }
+        {!isLoading && displayedData.map((tool) => 
+          <Card key={tool.name} tool={tool} 
+        />)}
+        {filteredData.length === 0 && (
+          <div className='flex items-center justify-center h-48 w-full text-4xl font-bold opacity-20 text-center'>
+            No Tools Found
+          </div>
+        )}
       </section>
+      {initialView && showAllButton && (
+          <button
+            onClick={showAllItems}
+            className='bg-[#161616] text-white px-4 py-2 rounded-md mt-4 mx-auto block'
+          >
+            Show All Items
+          </button>
+        )}
       <About />
     </main>
   );
