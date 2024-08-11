c0dd3aa485c6cdd6c80d7b0f014ea1d13fcdcd1d
Gerard Doncel <gerarddoncel41@gmail.com>
2024-08-11 19:29:18 +0200

    login and singin

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,15 +1,68 @@
-import Hero from '@/components/Hero';
-import About from '@/components/About';
-import ToolContent from '@/components/ToolContent';
+'use client'
+
+import { useState } from 'react';
+import axios from 'axios';
 
 export default function HomePage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+
+  const baseURL = 'http://192.168.1.48:3001';
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    setError('');
+
+    try {
+      const response = await axios.post(`${baseURL}/auth/signin`, { email, password });
+      console.log('Login successful:', response.data);
+    } catch (err) {
+      setError('Login failed. Please check your credentials and try again.');
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
-    <main className='max-w-6xl mx-auto px-9'>
-      <Hero />
-      <section className='flex flex-wrap gap-3 min-h-full mx-auto'>
-        <ToolContent />
-      </section>
-      <About />
+    <main className='max-w-6xl mx-auto px-9 flex flex-col items-center justify-center'>
+      <form onSubmit={handleSubmit} className='max-w-md mx-auto p-6 rounded-md shadow-md '>
+        {error && <p className='text-red-500 mb-4'>{error}</p>}
+        <div className='mb-4'>
+          <label htmlFor='email' className='block text-gray-700'>Email</label>
+          <input
+            type='email'
+            id='email'
+            className='w-full px-4 py-2 border rounded-md'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className='mb-6'>
+          <label htmlFor='password' className='block text-gray-700'>Password</label>
+          <input
+            type='password'
+            id='password'
+            className='w-full px-4 py-2 border rounded-md'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button
+          type='submit'
+          disabled={isSubmitting}
+          className={`w-full py-2 px-4 bg-blue-500 text-white rounded-md ${
+            isSubmitting ? 'opacity-50 cursor-not-allowed' : 'hover:bg-blue-600'
+          }`}
+        >
+          {isSubmitting ? 'Logging in...' : 'Login'}
+        </button>
+      </form>
     </main>
   );
 }
---
+++ src/app/signin/page.js
@@ -0,0 +1,107 @@
+'use client';
+
+import { useState } from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+
+export default function SignUpPage() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    setError('');
+
+    const baseURL = 'http://192.168.1.48:3001';
+
+    // Validación de la coincidencia de contraseñas
+    if (password !== confirmPassword) {
+      setError('Passwords do not match.');
+      setIsSubmitting(false);
+      return;
+    }
+
+    try {
+      const response = await axios.post(`${baseURL}/users`, {
+        username,
+        email,
+        password,
+      });
+
+      // Redirigir al usuario a otra página después de un registro exitoso
+      router.push('/login');
+    } catch (err) {
+      setError('Sign up failed. Please try again.');
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <form onSubmit={handleSubmit} className=' p-8 rounded-md shadow-md'>
+        <h2 className='text-2xl font-bold mb-6'>Sign Up</h2>
+        {error && <p className='text-red-500 mb-4'>{error}</p>}
+        <div className='mb-4'>
+          <label htmlFor='username' className='block text-gray-700'>Username</label>
+          <input
+            type='text'
+            id='username'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className='w-full px-4 py-2 border border-gray-300 rounded-md'
+            required
+          />
+        </div>
+        <div className='mb-4'>
+          <label htmlFor='email' className='block text-gray-700'>Email</label>
+          <input
+            type='email'
+            id='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className='w-full px-4 py-2 border border-gray-300 rounded-md'
+            required
+          />
+        </div>
+        <div className='mb-4'>
+          <label htmlFor='password' className='block text-gray-700'>Password</label>
+          <input
+            type='password'
+            id='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className='w-full px-4 py-2 border border-gray-300 rounded-md'
+            required
+          />
+        </div>
+        <div className='mb-4'>
+          <label htmlFor='confirmPassword' className='block text-gray-700'>Confirm Password</label>
+          <input
+            type='password'
+            id='confirmPassword'
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            className='w-full px-4 py-2 border border-gray-300 rounded-md'
+            required
+          />
+        </div>
+        <button
+          type='submit'
+          disabled={isSubmitting}
+          className={`w-full py-2 px-4 bg-green-500 text-white rounded-md ${
+            isSubmitting ? 'opacity-50 cursor-not-allowed' : 'hover:bg-green-600'
+          }`}
+        >
+          {isSubmitting ? 'Signing up...' : 'Sign Up'}
+        </button>
+      </form>
+    </div>
+  );
+}
