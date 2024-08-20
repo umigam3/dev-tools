1f0b45e6291af12c0ac20e16ecc58f44cf6b3aa5
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-08-20 22:57:15 +0200

    update login

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -1,21 +1,21 @@
-'use client'
+"use client";
 
 // Utility
-import axios from 'axios';
-import { useState } from 'react';
+import axios from "axios";
+import { useState } from "react";
 
 // UI
 import { Input } from "@nextui-org/react";
 import { Button } from "@nextui-org/button";
 
 // Icons
-import { EyeSlashFilledIcon } from '@/components/ui/EyeSlashFilledIcon';
-import { EyeFilledIcon } from '@/components/ui/EyeFilledIcon';
+import { EyeSlashFilledIcon } from "@/components/ui/EyeSlashFilledIcon";
+import { EyeFilledIcon } from "@/components/ui/EyeFilledIcon";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [isVisible, setIsVisible] = useState(false);
 
@@ -24,13 +24,16 @@ export default function LoginPage() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setIsSubmitting(true);
-    setError('');
+    setError("");
 
     try {
-      const response = await axios.post(`${baseURL}/auth/signin`, { email, password });
-      console.log('Login successful:', response.data);
+      const response = await axios.post(`${baseURL}/auth/signin`, {
+        email,
+        password,
+      });
+      console.log("Login successful:", response.data);
     } catch (err) {
-      setError('Login failed. Please check your credentials and try again.');
+      setError("Login failed. Please check your credentials and try again.");
     } finally {
       setIsSubmitting(false);
     }
@@ -39,63 +42,80 @@ export default function LoginPage() {
   const toggleVisibility = () => setIsVisible(!isVisible);
 
   return (
-    <main className='max-w-6xl mx-auto flex flex-col items-center min-h-[calc(100vh-112px)] py-20'>
-      <img src='/logo.svg' width="75" className='mb-2'/>
-      <span className="font-bold text-2xl mb-8">
-        AllTools
-      </span>
-      <form onSubmit={handleSubmit} className='flex flex-col justify-center items-center px-8 md:px-16 py-10 bg-[#161616] border-white/5 border rounded-2xl max-w-[30rem] w-full'>
-        <div className='flex flex-col gap-y-3 items-center mb-10'>
-          <h1 className='text-3xl font-bold'>Login</h1>
+    <main className="max-w-6xl mx-auto flex flex-col items-center min-h-[calc(100vh-112px)] py-20">
+      {/* <span className="font-bold text-2xl mb-8">AllTools</span> */}
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col justify-center items-center px-8 md:px-14 py-10 bg-[#161616] border-[#3F3F46]/10 border-0 rounded-xl max-w-[30rem] w-full"
+      >
+        <img src="/logo.svg" width="75" className="mb-2" />
+        <div className="flex flex-col gap-y-3 items-center mb-14">
+          <h1 className="text-2xl font-medium">Log in</h1>
         </div>
-        {error && <p className='text-red-500 mb-4'>{error}</p>}
-        <div className='mb-3 w-full'>
+        {error && <p className="text-red-500 mb-4">{error}</p>}
+        <div className="mb-3 w-full">
           <Input
-            color='dark'
-            className=''
-            variant='flat'
-            type='email'
-            label='Email'
-            placeholder="Enter your email"
-            id='email'
+            color="dark"
+            className=""
+            variant="bordered"
+            type="email"
+            label="Email"
+            size="md"
+            // placeholder="Enter your email"
+            id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            isRequired
             isClearable
+            onClear={() => {
+              setEmail("");
+            }}
+            isRequired
           />
         </div>
-        <div className='mb-6 w-full'>
+        <div className="mb-5 w-full">
           <Input
-            variant='flat'
+            variant="bordered"
             label="Password"
-            id='password'
+            id="password"
+            size="md"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            placeholder="Enter your password"
+            // placeholder="Enter your password"
             endContent={
-              <button className="focus:outline-none" type="button" onClick={toggleVisibility} aria-label="toggle password visibility">
-                {isVisible ? (
-                  <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                ) : (
-                  <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                )}
+              <button
+                className="focus:outline-none"
+                type="button"
+                onClick={toggleVisibility}
+                aria-label="toggle password visibility"
+              >
+                {password.length > 0 &&
+                  (isVisible ? (
+                    <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                  ) : (
+                    <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                  ))}
               </button>
             }
             type={isVisible ? "text" : "password"}
             isRequired
           />
         </div>
-        <div className='p-7 flex justify-center'>
-          <span className='text-center'>Don't have an accout? <a href='/signup' className='text-pink-500 hover:underline'>Sign Up</a></span>
-        </div>
-        <Button 
+        <Button
           isLoading={isSubmitting}
-          type='submit'
-          color='primary'
-          className='text-lg w-2/3 py-7 px-8 bg-gradient-to-r from-pink-600 to-purple-400 text-white rounded-xl'
+          type="submit"
+          color="primary"
+          className="text-lg w-full mb-16 py-6 px-8 bg-gradient-to-r from-pink-600 to-purple-400 text-white rounded-xxl font-medium"
         >
-          {isSubmitting ? 'Logging in...' : 'Login'}
+          {isSubmitting ? "Logging in..." : "Log in"}
         </Button>
+        <div className="w-full text-center mb-10">
+          <span className="text-sm">
+            No accout?{" "}
+            <a href="/signup" className="text-pink-500 hover:underline">
+              Sign Up
+            </a>
+          </span>
+        </div>
       </form>
     </main>
   );
--- src/app/(auth)/signup/page.js
+++ src/app/(auth)/signup/page.js
@@ -1,25 +1,24 @@
-'use client';
+"use client";
 
 // Utility
-import axios from 'axios';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
+import axios from "axios";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 // UI
 import { Input } from "@nextui-org/react";
 import { Button } from "@nextui-org/button";
 
 // Icons
-import { EyeSlashFilledIcon } from '@/components/ui/EyeSlashFilledIcon';
-import { EyeFilledIcon } from '@/components/ui/EyeFilledIcon';
-
+import { EyeSlashFilledIcon } from "@/components/ui/EyeSlashFilledIcon";
+import { EyeFilledIcon } from "@/components/ui/EyeFilledIcon";
 
 export default function SignUpPage() {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [error, setError] = useState('');
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [isVisible, setIsVisible] = useState(false);
   const router = useRouter();
@@ -27,12 +26,12 @@ export default function SignUpPage() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setIsSubmitting(true);
-    setError('');
+    setError("");
 
     const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 
     if (password !== confirmPassword) {
-      setError('Passwords do not match.');
+      setError("Passwords do not match.");
       setIsSubmitting(false);
       return;
     }
@@ -44,9 +43,9 @@ export default function SignUpPage() {
         password,
       });
 
-      router.push('/login');
+      router.push("/login");
     } catch (err) {
-      setError('Sign up failed. Please try again.');
+      setError("Sign up failed. Please try again.");
     } finally {
       setIsSubmitting(false);
     }
@@ -55,98 +54,120 @@ export default function SignUpPage() {
   const toggleVisibility = () => setIsVisible(!isVisible);
 
   return (
-    <main className='max-w-6xl mx-auto flex flex-col items-center min-h-[calc(100vh-112px)] py-6'>
-      <img src='/logo.svg' width="75" className='mb-2'/>
-      <span className="font-bold text-2xl mb-8">
-        AllTools
-      </span>
-      <form onSubmit={handleSubmit} className='flex flex-col justify-center items-center px-8 md:px-16 py-10 bg-[#161616] border-white/5 border rounded-2xl max-w-[30rem] mx-10 w-full'>
-        <div className='flex flex-col gap-y-3 items-center mb-10'>
-          <h1 className='text-3xl font-bold'>Sign Up</h1>
+    <main className="max-w-6xl mx-auto flex flex-col items-center min-h-[calc(100vh-112px)] py-6">
+      <img src="/logo.svg" width="75" className="mb-2" />
+      <span className="font-bold text-2xl mb-8">AllTools</span>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col justify-center items-center px-8 md:px-16 py-10 bg-[#161616] border-white/5 border rounded-2xl max-w-[30rem] mx-10 w-full"
+      >
+        <div className="flex flex-col gap-y-3 items-center mb-10">
+          <h1 className="text-3xl font-bold">Sign Up</h1>
         </div>
-        {error && <p className='text-red-500 mb-4'>{error}</p>}
-        <div className='mb-3 w-full'>
+        {error && <p className="text-red-500 mb-4">{error}</p>}
+        <div className="mb-3 w-full">
           <Input
-            color='dark'
-            className=''
-            variant='flat'
-            type='text'
-            label='Username'
-            placeholder='Enter your username'
-            id='username'
+            color="dark"
+            className=""
+            variant="flat"
+            type="text"
+            label="Username"
+            placeholder="Enter your username"
+            id="username"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
             isRequired
           />
         </div>
-        <div className='mb-3 w-full'>
+        <div className="mb-3 w-full">
           <Input
-            color='dark'
-            className=''
-            variant='flat'
-            type='email'
-            label='Email'
-            placeholder="Enter your email"
-            id='email'
+            color="dark"
+            className=""
+            variant="flat"
+            type="email"
+            label="Email"
+            // placeholder="Enter your email"
+            id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             isRequired
             isClearable
+            onClear={() => {
+              setEmail("");
+            }}
           />
         </div>
-        <div className='mb-3 w-full'>
+        <div className="mb-3 w-full">
           <Input
-            variant='flat'
+            variant="flat"
             label="Password"
-            id='password'
+            id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            placeholder="Enter your password"
+            // placeholder="Enter your password"
             endContent={
-              <button className="focus:outline-none" type="button" onClick={toggleVisibility} aria-label="toggle password visibility">
-                {isVisible ? (
-                  <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                ) : (
-                  <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                )}
+              <button
+                className="focus:outline-none"
+                type="button"
+                onClick={toggleVisibility}
+                aria-label="toggle password visibility"
+              >
+                {password.length > 0 &&
+                  (isVisible ? (
+                    <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                  ) : (
+                    <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                  ))}
               </button>
             }
             type={isVisible ? "text" : "password"}
             isRequired
           />
         </div>
-        <div className='mb-6 w-full'>
+        <div className="mb-6 w-full">
           <Input
-            variant='flat'
+            variant="flat"
             label="Password"
-            id='confirmPassword'
+            id="confirmPassword"
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
-            placeholder="Rpeat your password"
+            // placeholder="Rpeat your password"
+
             endContent={
-              <button className="focus:outline-none" type="button" onClick={toggleVisibility} aria-label="toggle password visibility">
-                {isVisible ? (
-                  <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                ) : (
-                  <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                )}
+              <button
+                className="focus:outline-none"
+                type="button"
+                onClick={toggleVisibility}
+                aria-label="toggle password visibility"
+              >
+                {confirmPassword.length > 0 &&
+                  (isVisible ? (
+                    <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                  ) : (
+                    <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                  ))}
               </button>
             }
             type={isVisible ? "text" : "password"}
             isRequired
           />
         </div>
-        <div className='p-7 flex justify-center'>
-          <span className='text-center'>Already have an account? <a href='/login' className='text-pink-500 hover:underline'>Login</a></span>
-        </div>
-        <Button 
+        <Button
           isLoading={isSubmitting}
-          type='submit'
-          color='primary'
-          className='text-lg w-2/3 py-7 px-8 bg-gradient-to-r from-pink-600 to-purple-400 text-white rounded-xl'
+          type="submit"
+          color="primary"
+          className="text-lg w-2/3 py-7 px-8 bg-gradient-to-r from-pink-600 to-purple-400 text-white rounded-xl"
         >
-          {isSubmitting ? 'Signing up...' : 'Sign Up'}
+          {isSubmitting ? "Signing up..." : "Create account"}
         </Button>
+        <div className="p-7 flex justify-center">
+          <span className="text-center">
+            Already have an account?{" "}
+            <a href="/login" className="text-pink-500 hover:underline">
+              Log in
+            </a>
+          </span>
+        </div>
       </form>
     </main>
   );
