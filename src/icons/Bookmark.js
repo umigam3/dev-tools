db8327672a764857733911aab7dca6b3d3293089
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-10-03 20:46:00 +0200

    Create Bookmark.js

---
+++ src/icons/Bookmark.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Bookmark({ className }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="22"
+      height="22"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      stroke="#f1f1f1"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className={className}
+    >
+      <path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z" />
+    </svg>
+  );
+}
+
+export default Bookmark;
