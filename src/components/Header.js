e645c5ea1b169bd93d5671704aa51cbc00b4a889
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-10-05 21:10:00 +0200

    add signUp

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import Login from "@/icons/LogIn";
 import { Button } from "@nextui-org/button";
 
-export default function Header({ setShowLogIn }) {
+export default function Header({ setShowLogIn, setShowSignUp }) {
   return (
-    <header className="fixed z-50 flex items-center h-16 w-full bg-[#121212] header">
+    <header className="fixed z-40 flex items-center h-16 w-full bg-[#121212] header">
       <div className="flex justify-between max-w-7xl w-full mx-auto px-9">
         <a
           href="/"
@@ -13,16 +13,29 @@ export default function Header({ setShowLogIn }) {
           Dev Tools
         </a>
 
-        <button
-          onClick={() => setShowLogIn(true)}
-          // className="bg-gradient-to-r from-pink-600 to-purple-400 inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-[#f1f1f1] dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800 w-36 max-w-36"
-          className="relative group inline-flex justify-center items-center py-1 px-5 w-30 w-max-30 text-base font-medium text-center text-[#f1f1f1] rounded-[0.6rem] focus:ring-primary-300 hover:bg-white/15 p-4"
+        <div className="flex flex-row gap-x-4">
+          <button
+            onClick={() => setShowLogIn(true)}
+            // className="bg-gradient-to-r from-pink-600 to-purple-400 inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-[#f1f1f1] dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800 w-36 max-w-36"
+            className="relative group inline-flex justify-center items-center py-1 px-5 w-30 w-max-30 text-base font-medium text-center text-[#f1f1f1] rounded-[0.6rem] focus:ring-primary-300 hover:bg-white/15 p-4"
 
-          // className="text-lg w-full px-8 bg-gradient-to-r from-[#DB2677] to-[#b24dda] text-[#f1f1f1] rounded-xxl font-medium"
-        >
-          {/* <img src="/user.svg" width="22px" className="mr-1" /> */}
-          Sign in
-        </button>
+            // className="text-lg w-full px-8 bg-gradient-to-r from-[#DB2677] to-[#b24dda] text-[#f1f1f1] rounded-xxl font-medium"
+          >
+            {/* <img src="/user.svg" width="22px" className="mr-1" /> */}
+            Sign in
+          </button>
+          <button
+            onClick={() => setShowSignUp(true)}
+            // className="bg-gradient-to-r from-pink-600 to-purple-400 inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-[#f1f1f1] dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800 w-36 max-w-36"
+            className="relative group inline-flex justify-center items-center py-1 px-7 w-30 w-max-30 text-base font-medium text-center text-[#f1f1f1] rounded-[0.6rem] focus:ring-primary-300 bg-gradient"
+
+            // className="text-lg w-full px-8 bg-gradient-to-r from-[#DB2677] to-[#b24dda] text-[#f1f1f1] rounded-xxl font-medium"
+          >
+            {/* <img src="/user.svg" width="22px" className="mr-1" /> */}
+            Join
+          </button>
+        </div>
+
         {/* <a href="/login"> */}
         {/* <Login className="h-6 w-6 hover:scale-125 duration-150" /> */}
         {/* </a> */}
---
+++ src/components/SignUp.jsx
@@ -0,0 +1,274 @@
+"use client";
+
+// Utility
+import axios from "axios";
+import { useState } from "react";
+
+// UI
+import { Input } from "@nextui-org/react";
+import { Button } from "@nextui-org/button";
+import Close from "@/icons/Close";
+
+// Icons
+import { EyeSlashFilledIcon } from "@/components/ui/EyeSlashFilledIcon";
+import { EyeFilledIcon } from "@/components/ui/EyeFilledIcon";
+
+// Framer Motion
+import { motion } from "framer-motion";
+
+export default function SignUp({ setShowSignUp, setShowLogIn }) {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    setError("");
+
+    const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+
+    try {
+      const response = await axios.post(`${baseURL}/users`, {
+        username,
+        email,
+        password,
+      });
+
+      router.push("/login");
+    } catch (err) {
+      setError("Sign up failed. Please try again.");
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  const toggleVisibility = () => setIsVisible(!isVisible);
+  return (
+    <motion.div
+      className="flex flex-row justify-center z-40 fixed top-0 h-[100vh] w-full backdrop-blur-[0px] bg-black/80"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{
+        opacity: 0,
+
+        transition: {
+          opacity: {
+            ease: [0.32, 0, 0.67, 0],
+            duration: 0.25, // exit transition
+          },
+        },
+      }}
+      transition={{ ease: [0.33, 1, 0.68, 1], duration: 0.5 }}
+    >
+      <motion.form
+        onSubmit={handleSubmit}
+        className=" flex flex-col justify-center items-center px-8 md:px-14 pb-10 my-48 modal border-[#3F3F46]/10 border-0 rounded-2xl max-w-[25rem] w-full"
+        initial={{ scale: 0.95, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        exit={{
+          scale: 0.95,
+          opacity: 0,
+
+          transition: {
+            opacity: {
+              ease: [0.32, 0, 0.67, 0],
+              duration: 0.25, // exit transition
+            },
+          },
+        }}
+        transition={{
+          ease: [0.33, 1, 0.68, 1],
+          duration: 0.25, // animate transition
+        }}
+      >
+        {/* <img src="/logo.svg" width="75" className="mb-1" /> */}
+        <button
+          type="button"
+          onClick={() => setShowSignUp(false)}
+          className="hover:bg-white/15 rounded-full p-2 absolute top-4 right-5"
+        >
+          <Close className="text-gray-100" />
+        </button>
+        <div className="flex flex-col gap-y-3 items-center mb-10 mt-16">
+          <h1 className="text-title font-bold font-space_grotesk">
+            Join Dev Tools!
+          </h1>
+        </div>
+        {error && <p className="text-red-500 mb-4">{error}</p>}
+        <div className="mb-3 w-11/12 z-30 relative">
+          <Input
+            color="dark"
+            variant="bordered"
+            type="username"
+            label="Username"
+            size="sm"
+            radius="md"
+            // placeholder="Enter your email"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            // isClearable
+            // onClear={() => {
+            //   setEmail("");
+            // }}
+            // isRequired
+            classNames={{
+              input: ["text-base"],
+              mainWrapper: ["text-base"],
+              label: [
+                "text-base",
+                "text-gray-200",
+                "absolute",
+                "transition-all",
+                "px-2",
+                // "rounded-full",
+                "duration-300",
+                "bg-[#1f1f1f]",
+                "group-data-[filled-within=true]:-translate-y-[calc(80%_+_theme(fontSize.tiny)/2_-_8px_-_theme(borderWidth.medium))]",
+              ],
+
+              innerWrapper: ["group-data-[has-label=true]:items-center"],
+              inputWrapper: [
+                "border-[#525252]",
+                "group-data-[focus=true]:border-[#D50E97]",
+                "group-data-[focus=true]:border-2",
+                "border-1",
+                "py-0",
+                "text-2xl",
+              ],
+              input: ["pl-2", "text-base"],
+            }}
+          />
+        </div>
+        <div className="mb-3 w-11/12 z-20 relative">
+          <Input
+            color="dark"
+            variant="bordered"
+            type="email"
+            label="Email"
+            size="sm"
+            radius="md"
+            // placeholder="Enter your email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            // isClearable
+            // onClear={() => {
+            //   setEmail("");
+            // }}
+            // isRequired
+            classNames={{
+              input: ["text-base"],
+              mainWrapper: ["text-base"],
+              label: [
+                "text-base",
+                "text-gray-200",
+                "absolute",
+                "transition-all",
+                "px-2",
+                // "rounded-full",
+                "duration-300",
+                "bg-[#1f1f1f]",
+                "group-data-[filled-within=true]:-translate-y-[calc(80%_+_theme(fontSize.tiny)/2_-_8px_-_theme(borderWidth.medium))]",
+              ],
+
+              innerWrapper: ["group-data-[has-label=true]:items-center"],
+              inputWrapper: [
+                "border-[#525252]",
+                "group-data-[focus=true]:border-[#D50E97]",
+                "group-data-[focus=true]:border-2",
+                "border-1",
+                "py-0",
+                "text-2xl",
+              ],
+              input: ["pl-2", "text-base"],
+            }}
+          />
+        </div>
+        <div className="mb-10 w-11/12">
+          <Input
+            variant="bordered"
+            label="Password"
+            id="password"
+            size="sm"
+            radius="md"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            // placeholder="Enter your password"
+            endContent={
+              <button
+                className="focus:outline-none"
+                type="button"
+                onClick={toggleVisibility}
+                aria-label="toggle password visibility"
+              >
+                {password.length > 0 &&
+                  (isVisible ? (
+                    <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                  ) : (
+                    <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                  ))}
+              </button>
+            }
+            type={isVisible ? "text" : "password"}
+            // isRequired
+            classNames={{
+              mainWrapper: ["text-base"],
+              label: [
+                "text-base",
+                "text-gray-200",
+                "absolute",
+                "transition-all",
+                "px-2",
+                // "rounded-full",
+                "duration-300",
+                "bg-[#1f1f1f]",
+                "group-data-[filled-within=true]:-translate-y-[calc(80%_+_theme(fontSize.tiny)/2_-_8px_-_theme(borderWidth.medium))]",
+              ],
+
+              innerWrapper: ["group-data-[has-label=true]:items-center"],
+              inputWrapper: [
+                "border-[#525252]",
+                "group-data-[focus=true]:border-[#D50E97]",
+                "group-data-[focus=true]:border-2",
+                "border-1",
+                "py-0",
+                "text-2xl",
+              ],
+              input: ["pl-2", "text-base"],
+            }}
+          />
+        </div>
+        <Button
+          isLoading={isSubmitting}
+          type="submit"
+          color="primary"
+          className="text-base w-11/12 mb-6 py-6 px-8 bg-gradient text-[#f1f1f1] rounded-xxl font-medium"
+        >
+          {isSubmitting ? "Creating..." : "Create account"}
+        </Button>
+        <div className="w-full text-center mb-6">
+          <span className="text-[13px]">
+            Already have an account?{" "}
+            <button
+              type="button"
+              onClick={() => {
+                setShowSignUp(false);
+                setTimeout(() => {
+                  setShowLogIn(true);
+                }, 300);
+              }}
+              className="text-pink-500 underline"
+            >
+              Sign in
+            </button>
+          </span>
+        </div>
+      </motion.form>
+    </motion.div>
+  );
+}
