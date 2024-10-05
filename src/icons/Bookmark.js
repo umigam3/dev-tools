a6b31a42bec747648cba6322ab723a102888ec0b
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-10-05 21:36:00 +0200

    fix compatibility svg/jsx

--- src/icons/Bookmark.js
+++ src/icons/Bookmark.js
@@ -9,9 +9,9 @@ function Bookmark({ className }) {
       viewBox="0 0 24 24"
       fill="currentColor"
       stroke="#f1f1f1"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className={className}
     >
       <path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z" />
