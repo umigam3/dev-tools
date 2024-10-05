0031502796e084be596153f332ca64846e98deb4
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-10-05 18:31:00 +0200

    add login modal logic

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,11 @@
+"use client";
+
 import { Space_Grotesk, DM_Sans, Barlow } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import LogIn from "@/components/LogIn";
 import "./globals.css";
+import { useState } from "react";
 
 const dm_sans = DM_Sans({ subsets: ["latin"], variable: "--font-dm_sans" });
 const space_grotesk = Space_Grotesk({
@@ -9,24 +13,29 @@ const space_grotesk = Space_Grotesk({
   variable: "--font-space_grotesk",
 });
 
-export const metadata = {
-  title: "AllTools.dev - A complete collection of developer tools",
-  description:
-    "A collection of coding tools designed to simplify developers lives and boost productivity.",
-  icons: {
-    icon: "./favicon.ico",
-  },
-};
+// export const metadata = {
+//   title: "AllTools.dev - A complete collection of developer tools",
+//   description:
+//     "A collection of coding tools designed to simplify developers lives and boost productivity.",
+//   icons: {
+//     icon: "./favicon.ico",
+//   },
+// };
 
 export default function RootLayout({ children }) {
+  const [showLogIn, setShowLogIn] = useState(false);
   return (
     <html lang="en" className="dark">
       <body
         className={`${dm_sans.variable} ${space_grotesk.variable} font-dm_sans`}
       >
-        <Header />
+        {/* <LogInContext.Provider value={setShowLogIn}> */}
+        <Header setShowLogIn={setShowLogIn} />
+        {showLogIn && <LogIn />}
+
         {children}
         <Footer />
+        {/* </LogInContext.Provider> */}
       </body>
     </html>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import Login from "@/icons/LogIn";
 import { Button } from "@nextui-org/button";
 
-export default function Header() {
+export default function Header({ setShowLogIn }) {
   return (
-    <header className="flex items-center h-20 w-full">
+    <header className="fixed z-50 flex items-center h-20 w-full bg-[#121212]">
       <div className="flex justify-between max-w-7xl w-full mx-auto px-9">
         <a
           href="/"
@@ -13,8 +13,8 @@ export default function Header() {
           Dev Tools
         </a>
 
-        <a
-          href="/login"
+        <button
+          onClick={() => setShowLogIn(true)}
           // className="bg-gradient-to-r from-pink-600 to-purple-400 inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-[#f1f1f1] dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800 w-36 max-w-36"
           className="relative group inline-flex justify-center items-center py-1 px-5 w-30 w-max-30 text-base font-medium text-center bg-transparent text-[#f1f1f1] rounded-lg focus:ring-4 focus:ring-primary-300 hover:text-gradient hover:font-black hover:tracking-tight"
 
@@ -22,7 +22,7 @@ export default function Header() {
         >
           {/* <img src="/user.svg" width="22px" className="mr-1" /> */}
           Sign in
-        </a>
+        </button>
         {/* <a href="/login"> */}
         {/* <Login className="h-6 w-6 hover:scale-125 duration-150" /> */}
         {/* </a> */}
