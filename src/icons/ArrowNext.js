687238b47b94f51ce4f6a290710827f4dce96a1d
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-10-07 20:47:00 +0200

    Create ArrowNext.js

---
+++ src/icons/ArrowNext.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function ArrowNext({ className }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+    >
+      <path d="M5 12h14" />
+      <path d="m12 5 7 7-7 7" />
+    </svg>
+  );
+}
+
+export default ArrowNext;
