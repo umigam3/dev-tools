2c4cc549a113f2da63f3ca2868c16b91e9699fd7
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-10-15 18:17:37 +0200

    add side buttons

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -11,18 +11,76 @@ import {
   DropdownItem,
   Button,
 } from "@nextui-org/react";
+import HoverButton from "../components/HoverButton";
 
 import User from "../icons/User";
-import Close from "../icons/Close";
+import Home from "../icons/Home";
+import Featured from "../icons/Featured";
+import Users from "../icons/Users";
+import Collections from "../icons/Collections";
+import { useState } from "react";
 
 export default function SideBar() {
-  const session = false;
+  const [selected, setSelected] = useState(0);
   const router = useRouter();
 
   return (
-    <div className="fixed z-40 flex items-start justify-center w-16 h-full bg-[#c53535] header pt-10">
-      <div className="flex flex-col justify-between">
-        <Close className="text-gray-100" />
+    <div className="fixed z-40 flex items-start justify-center w-16 h-full header pt-16 pl-4">
+      <div className="flex flex-col justify-between gap-y-2">
+        <HoverButton
+          className={
+            "p-2 w-16 h-16 rounded-xl relative flex flex-col items-center justify-center"
+          }
+          icon={
+            <Home
+              className="text-gray-100"
+              fill={selected === 0 ? "#f3f4f6" : "none"}
+            />
+          }
+          onClick={() => setSelected(0)}
+          text={"Home"}
+        />
+
+        <HoverButton
+          className={
+            "p-2 w-16 h-16 rounded-xl relative flex flex-col items-center justify-center"
+          }
+          icon={
+            <Featured
+              className="text-gray-100"
+              fill={selected === 1 ? "#f3f4f6" : "none"}
+            />
+          }
+          onClick={() => setSelected(1)}
+          text={"Featured"}
+        />
+
+        <HoverButton
+          className={
+            "p-2 w-16 h-16 rounded-xl relative flex flex-col items-center justify-center"
+          }
+          icon={
+            <Collections
+              className="text-gray-100"
+              fill={selected === 3 ? "#f3f4f6" : "none"}
+            />
+          }
+          onClick={() => setSelected(3)}
+          text={"Collections"}
+        />
+        {/* <HoverButton
+          className={
+            "p-2 w-16 h-16 rounded-xl relative flex flex-col items-center justify-center"
+          }
+          icon={
+            <Users
+              className="text-gray-100"
+              fill={selected === 2 ? "#f3f4f6" : "none"}
+            />
+          }
+          onClick={() => setSelected(2)}
+          text={"Users"}
+        /> */}
       </div>
     </div>
   );
