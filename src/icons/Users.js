bd8e5af5a1b335b743e35ae6762c3603075ba3d7
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-10-15 17:54:05 +0200

    update icons fill

--- src/icons/Users.js
+++ src/icons/Users.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-function Users({ className }) {
+function Users({ className, fill }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
       viewBox="0 0 24 24"
-      fill="none"
+      fill={fill}
       stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
