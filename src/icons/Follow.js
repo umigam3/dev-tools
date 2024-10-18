dcbd19691e563d33bfd20a3dcf91566177abd8b6
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-10-18 14:13:32 +0200

    Create Follow.js

---
+++ src/icons/Follow.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Follow({ className }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2.5"
+      className={className}
+      viewBox="0 0 24 24"
+    >
+      <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+      <circle cx="9" cy="7" r="4" />
+      <line x1="19" x2="19" y1="8" y2="14" />
+      <line x1="22" x2="16" y1="11" y2="11" />
+    </svg>
+  );
+}
+
+export default Follow;
