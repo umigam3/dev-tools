36e7a9d0fa31912d4e5209e4569aae4aa310d010
Noah Guardiola <58909117+Blondie-TheManWithNoName@users.noreply.github.com>
2024-10-19 12:47:22 +0200

    Create ArrowDown.js

---
+++ src/icons/ArrowDown.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function ArrowDown({ className }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className={className}
+      viewBox="0 0 24 24"
+    >
+      <path d="m6 9 6 6 6-6" />
+    </svg>
+  );
+}
+
+export default ArrowDown;
